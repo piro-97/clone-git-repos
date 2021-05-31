f89faff28b687a0fe5332124e8cc48d5e01db255
piro-97 <mpirola97@gmail.com>
2021-05-31 08:43:41 +0200

    added the possibility to download other users repo

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
 import argparse
+import re
 
-parser = argparse.ArgumentParser(description='Clone a list of git repositories; fill repos.txt with the names of the repos you want to clone')
 
-parser.add_argument("github-username", help='username of your github account')
+def find_repo_name(url):
+    """
+        from url: 'https://github.com/<github_username>/<repo_name>'
+        returns (<github_username>, <repo_name>)
+    """
+    names = url.split("/")  # github_username = names[3]; repo_name = names[4]
+    
+    if len(names) < 5:
+        print("Invalid url supplied: ", url)
+        return ("", "")
+
+    return ( names[3], names[4] )
+
+
+# parsing argv
+description = 'Clone a list of git repositories; fill repos.txt with the urls of the repos you want to clone; note that you must be logged in in your github account via SSH'
+parser = argparse.ArgumentParser(description=description)
 parser.add_argument("--output-dir", help='folder in which the repos will be cloned')
 
 args = vars( parser.parse_args() )
@@ -23,13 +39,15 @@ except FileNotFoundError:
     os.chdir(out_path)
 
 
-url_base = "git@github.com:" + args["github-username"] +"/"     # base url for git repos (SSH)
+# actual cloning
+url_base = "git@github.com:"     # base url for git repos (SSH)
 try:
-    with open("repos.txt", "r") as f:
-        for repo_name in f.read().split():   # reading and splitting repo names
-            repo_url = url_base + repo_name
-            os.system("git clone " + repo_url + ".git") # running git clone command
+    with open("repo_urls.txt", "r") as f:
+        for line in f.readlines():
+            name = find_repo_name(line)
+            repo_url = url_base + name[0] + "/" + name[1] + ".git"
+            os.system("git clone " + repo_url) # running git clone command
 
 except FileNotFoundError:
-    os.system("touch repos.txt")    # creating empty repos.txt
-    print("repos.txt is empty, please insert at least one repo name")
+    os.system("touch repo_urls.txt")    # creating empty repo_urls.txt
+    print("repo_urls.txt is empty, please insert at least one repo url")
